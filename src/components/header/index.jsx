9c92e26ec7dc344afaab297933e97bca1d37133c
JuniorMoura10 <geraldomartins10@gmail.com>
2022-03-04 13:51:15 -0300

    initial commit

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -9,17 +9,32 @@ import {
     HeaderSearchButton
 } from './styles';
 
-const Header = () => (
-    <HeaderSection>
-        <HeaderTitle>Github Profile</HeaderTitle>
-        <HeaderInputContainer>
-            <HeaderInput />
-            
-            <HeaderSearchButton>
-                <FiSearch size={15} />
-            </HeaderSearchButton>
-        </HeaderInputContainer>
-    </HeaderSection>
-);
+import client from '../../services/client';
+
+const Header = () => {
+    const [searchedValue, setSearchedValue] = useState('');
+
+    async function getUserData(){
+        try {
+           const response = await client.get(`/${searchedValue}`);
+           console.log(response.data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <HeaderSection>
+            <HeaderTitle>Github Profile</HeaderTitle>
+            <HeaderInputContainer>
+                <HeaderInput value={searchedValue} onChange={e => setSearchedValue(e.target.value)} />
+                
+                <HeaderSearchButton onClick={getUserData}>
+                    <FiSearch size={15} />
+                </HeaderSearchButton>
+            </HeaderInputContainer>
+        </HeaderSection>
+    );
+}
 
 export default Header;
