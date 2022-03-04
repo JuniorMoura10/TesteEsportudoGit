ef51ec85ed876185f95881bf64591c9e811aa9b4
JuniorMoura10 <geraldomartins10@gmail.com>
2022-03-04 17:50:54 -0300

    atualização

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import Routes from './Routes';
-import { ContextProvider } from './context';
+import Routes from './src/Routes';
+import { ContextProvider } from './src/context';
 
 const App = () => (
     <ContextProvider>
